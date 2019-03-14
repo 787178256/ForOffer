4dd722966ec66d33882f2354670db21908b0c8b7
kimvra <787178256@qq.com>
2019-03-14 22:35:11 +0800

    leetcode 3.14

---
+++ src/leetcode/EPractice485.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+/**
+ * Created by kimvra on 2019-03-14
+ */
+public class EPractice485 {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int len = nums.length;
+        int max = 0;
+        for (int i = 0; i < len;) {
+            int count = 0;
+            if (nums[i] != 1) {
+                i++;
+                continue;
+            }
+            while (i < len && nums[i] == 1) {
+                i++;
+                count++;
+            }
+            max = Math.max(count, max);
+        }
+        return max;
+    }
+
+    public static int findMaxConsecutiveOnes1(int[] nums) {
+        int cur = 0, max = 0;
+        for (int num : nums) {
+            cur = num == 0 ? 0 : cur + 1;
+            max = Math.max(max, cur);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+}
---
+++ src/leetcode/Interval.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * Created by kimvra on 2019-03-14
+ */
+public class Interval {
+    private int start;
+    private int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int s, int e) {
+        start = s;
+        end = end;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+}
