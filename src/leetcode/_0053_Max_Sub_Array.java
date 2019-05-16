fa94d495db65db5e209a531dd9242b6bbf3575a6
kimvra <787178256@qq.com>
2019-05-16 11:56:16 +0800

    leectode code review

--- src/leetcode/_0053_Max_Sub_Array.java
+++ src/leetcode/_0053_Max_Sub_Array.java
@@ -4,7 +4,7 @@ package leetcode;
  * Created by kimvra on 2019-03-13
  */
 public class _0053_Max_Sub_Array {
-    public int maxSubArray(int[] nums) {
+    private static int maxSubArray(int[] nums) {
         int len = nums.length, dp = nums[0], max = dp;
         for (int i = 1; i < len; i++) {
             dp = nums[i] + (dp > 0 ? dp : 0);
@@ -13,6 +13,11 @@ public class _0053_Max_Sub_Array {
             }
         }
         return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
     }
 
 }
