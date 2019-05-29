00517fd848ffeae475fe72821e4f89ef4988bc4c
kimvra <787178256@qq.com>
2019-05-29 22:36:14 +0800

    leetcode 0072 5.29 22:36 copy from ivan_allen

---
+++ src/leetcode/_0072_Min_Distance.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+/**
+ * Created by kimvra on 2019-05-29
+ */
+public class _0072_Min_Distance {
+    public static int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] cost = new int[m+1][n+1];//cost[i][j]表示word1[0...i-1]到word2[0...j-1]的步数
+        for (int i = 0; i <= m; i++) {
+            cost[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            cost[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    cost[i][j] = cost[i-1][j-1];
+                } else {
+                    cost[i][j] = 1 + Math.min(cost[i-1][j-1], Math.min(cost[i][j-1], cost[i-1][j]));
+                }
+            }
+        }
+        return cost[m][n];
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(minDistance("horse", "ros"));
+//        System.out.println(minDistance("intention", "execution"));
+        System.out.println(minDistance("", "a"));
+    }
+}
