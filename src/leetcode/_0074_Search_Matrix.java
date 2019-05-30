b417b0738f44adc07d945de63e097ebf9695c126
kimvra <787178256@qq.com>
2019-05-30 11:15:40 +0800

    leetcode 0074 5.30 11:15 by kimvra

---
+++ src/leetcode/_0074_Search_Matrix.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+/**
+ * Created by kimvra on 2019-05-30
+ * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+ *
+ * 每行中的整数从左到右按升序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ * 示例 1:
+ *
+ * 输入:
+ * matrix = [
+ *   [1,   3,  5,  7],
+ *   [10, 11, 16, 20],
+ *   [23, 30, 34, 50]
+ * ]
+ * target = 3
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入:
+ * matrix = [
+ *   [1,   3,  5,  7],
+ *   [10, 11, 16, 20],
+ *   [23, 30, 34, 50]
+ * ]
+ * target = 13
+ * 输出: false
+ */
+public class _0074_Search_Matrix {
+    public  boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int startX = rows - 1, startY = 0;
+        while (startX >= 0 && startX < rows && startY >= 0 && startY < cols) {
+            if (target > matrix[startX][startY]) {
+                startY++;
+            } else if (target < matrix[startX][startY]) {
+                startX--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
