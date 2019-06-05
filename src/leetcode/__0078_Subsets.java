d0bb489517e933cb1067d5c0b91d1005b226cf84
kimvra <787178256@qq.com>
2019-06-05 17:00:28 +0800

    leetcode 0078 6.5 17:00

---
+++ src/leetcode/__0078_Subsets.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kimvra on 2019-06-05
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ *
+ * 说明：解集不能包含重复的子集。
+ * 示例:
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ *   [3],
+ *   [1],
+ *   [2],
+ *   [1,2,3],
+ *   [1,3],
+ *   [2,3],
+ *   [1,2],
+ *   []
+ * ]
+ */
+public class __0078_Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> list = new ArrayList<>(res.get(j));
+                list.add(nums[i]);
+                res.add(list);
+            }
+        }
+        return res;
+    }
+}
