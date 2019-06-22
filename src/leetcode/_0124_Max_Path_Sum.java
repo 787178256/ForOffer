7ee191120d7eef07f65167fd853a64ff35550ebe
kimvra <787178256@qq.com>
2019-06-22 11:13:30 +0800

    *** leetcode 0124 6.22 11:13 review ***

--- src/leetcode/_0124_Max_Path_Sum.java
+++ src/leetcode/_0124_Max_Path_Sum.java
@@ -23,8 +23,8 @@ public class _0124_Max_Path_Sum {
         }
         int left = Math.max(helper(root.left), 0);
         int right = Math.max(helper(root.right), 0);
-        max = Math.max(max, left + root.val + right);
-        return Math.max(left, right) + root.val;
+        max = Math.max(max, root.val + left + right);
+        return root.val + Math.max(left, right);
     }
 
     @Before
