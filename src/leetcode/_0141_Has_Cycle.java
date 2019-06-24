8fe96b414419579d50d62e1613db91402a9ea356
kimvra <787178256@qq.com>
2019-06-24 19:32:32 +0800

    leetcode 0141 6.24 19:32

---
+++ src/leetcode/_0141_Has_Cycle.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by kimvra on 2019-06-24
+ * 给定一个链表，判断链表中是否有环。
+ */
+public class _0141_Has_Cycle {
+    private boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Test
+    public void test() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = head;
+        Assert.assertTrue(hasCycle(head));
+    }
+}
