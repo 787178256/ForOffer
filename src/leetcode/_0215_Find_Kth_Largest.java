b8c53d91d3c728e478b84dfbf73d0162dc538442
kimvra <787178256@qq.com>
2019-07-12 22:37:05 +0800

    Leetcode_ByteDance_FindKth 7.12 22:35

--- src/leetcode/_0215_Find_Kth_Largest.java
+++ src/leetcode/_0215_Find_Kth_Largest.java
@@ -28,9 +28,48 @@ public class _0215_Find_Kth_Largest {
         return heap.poll();
     }
 
+    private int findKthLargest2(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || nums.length < k) {
+            return -1;
+        }
+        int len = nums.length;
+        int start = 0, end = len - 1;
+        int index = getIndex(nums, start, end);
+        while (index != len - k && start <= end) {
+            if (index < len - k) {
+                start = index + 1;
+                index = getIndex(nums, start, end);
+            } else {
+                end = index - 1;
+                index = getIndex(nums, start, end);
+            }
+        }
+        return nums[index];
+    }
+
+    private int getIndex(int[] nums, int start, int end) {
+        int pivot = nums[start];
+        swap(nums, start, end);
+        int curIndex = start;
+        for (int i = start; i <= end; i++) {
+            if (nums[i] < pivot) {
+                swap(nums, i, curIndex);
+                curIndex++;
+            }
+        }
+        swap(nums, curIndex, end);
+        return curIndex;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
     @Test
     public void test() {
-        Assert.assertEquals(findKthLargest(new int[]{3,2,1,5,6,4}, 2), 5);
-        Assert.assertEquals(findKthLargest(new int[]{3,2,3,1,2,4,5,5,6}, 2), 5);
+        Assert.assertEquals(getIndex(new int[]{3,2,1,5,6,4}, 0, 5), 2);
+        Assert.assertEquals(findKthLargest2(new int[]{3,2,1,5,6,4}, 2), 5);
+        Assert.assertEquals(findKthLargest2(new int[]{3,2,3,1,2,4,5,5,6}, 2), 5);
     }
 }
