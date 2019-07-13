62a0ff06975082b213d4dd4a56232e0f7f6b31a3
kimvra <787178256@qq.com>
2019-07-13 21:14:49 +0800

    Leetcode_ByteDance_GetPermutation 7.13 21:14

--- src/leetcode/_0060_Get_Permutation.java
+++ src/leetcode/_0060_Get_Permutation.java
@@ -59,7 +59,77 @@ public class _0060_Get_Permutation {
         }
     }
 
+    public String getPermutation1(int n, int k) {
+        int[] nums = new int[n];
+        boolean[] used = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = i + 1;
+            used[i] = false;
+        }
+        List<String> pre = new ArrayList<>();
+        return dfs(nums, used, n, k, 0, pre);
+    }
+
+    private int factorial(int n) {
+        // 这种编码方式包括了 0 的阶乘是 1 这种情况
+        int res = 1;
+        while (n > 0) {
+            res *= n;
+            n -= 1;
+        }
+        return res;
+    }
+
+    private String dfs(int[] nums, boolean[] used, int n, int k, int depth, List<String> pre) {
+        if (depth == n) {
+            StringBuilder sb = new StringBuilder();
+            for (String c : pre) {
+                sb.append(c);
+            }
+            return sb.toString();
+        }
+        int ps = factorial(n - 1 - depth);
+        for (int i = 0; i < n; i++) {
+            if (used[i]) {
+                continue;
+            }
+            if (ps < k) {
+                k -= ps;
+                continue;
+            }
+            pre.add(nums[i] + "");
+            used[i] = true;
+            return dfs(nums, used, n, k, depth + 1, pre);
+        }
+        // 如果参数正确的话，代码不会走到这里
+        throw new RuntimeException("参数错误");
+    }
+
+    private String review(int[] nums, boolean[] used, int n, int k, int depth, List<String> pre) {
+        if (depth == n) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String s : pre) {
+                stringBuilder.append(s);
+            }
+            return stringBuilder.toString();
+        }
+        int ps = factorial(n - 1 - depth);
+        for (int i = 0; i < n; i++) {
+            if (used[i]) {
+                continue;
+            }
+            if (k > ps) {
+                k -= ps;
+                continue;
+            }
+            pre.add(nums[i] + "");
+            used[i] = true;
+            return dfs(nums, used, n, k, depth + 1, pre);
+        }
+        throw new RuntimeException("");
+    }
+
     public static void main(String[] args) {
-        System.out.println(getPermutation(4, 9));
+        //System.out.println(getPermutation(4, 9));
     }
 }
