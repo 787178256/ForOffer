12a28f344599b3a78b4ad634378de3a17613c253
kimvra <787178256@qq.com>
2019-07-17 16:01:34 +0800

    leetcode0236_LCA 7.17 16:01

--- src/leetcode/_0236_Lowest_Common_Ancestor.java
+++ src/leetcode/_0236_Lowest_Common_Ancestor.java
@@ -1,6 +1,10 @@
 package leetcode;
 
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by kimvra on 2019-03-22
  */
@@ -14,6 +18,11 @@ class TreeNode {
     }
 }
 public class _0236_Lowest_Common_Ancestor {
+
+    private HashMap<TreeNode, TreeNode> parent = new HashMap<>();
+
+    private HashMap<TreeNode, Integer> level = new HashMap<>();
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) {
             return null;
@@ -36,5 +45,63 @@ public class _0236_Lowest_Common_Ancestor {
             return right;
         }
         return null;
+    }
+
+    private TreeNode review(TreeNode root, TreeNode p, TreeNode q) {
+        if (p == q) {
+            return q;
+        }
+        helper(root);
+        int pLevel = level.get(p);
+        int qLevel = level.get(q);
+        if (qLevel > pLevel) {
+            int h = qLevel - pLevel;
+            while (h > 0) {
+                q = parent.get(q);
+                h--;
+            }
+        }else if (qLevel < pLevel) {
+            int h = pLevel - qLevel;
+            while (h > 0) {
+                p = parent.get(p);
+                h--;
+            }
+        }
+        if (q == p) {
+            return q;
+        }
+        while (parent.get(p) != parent.get(q)) {
+            p = parent.get(p);
+            q = parent.get(q);
+        }
+        return parent.get(p);
+    }
+
+    private void helper(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        parent.put(root, null);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int l = 1;
+        int c = 1;
+        while (!queue.isEmpty()) {
+            c--;
+            TreeNode node = queue.poll();
+            level.put(node, l);
+            if (node.left != null) {
+                queue.add(node.left);
+                parent.put(node.left, node);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+                parent.put(node.right, node);
+            }
+            if (c == 0) {
+                c = queue.size();
+                l++;
+            }
+        }
     }
 }
