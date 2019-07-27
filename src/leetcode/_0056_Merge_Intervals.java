c80f2593caf012ccee091f00bb1e0d3cf1225e66
kimvra <787178256@qq.com>
2019-07-27 19:26:29 +0800

    leetcode 0056 合并区间 7.26 19:26

--- src/leetcode/_0056_Merge_Intervals.java
+++ src/leetcode/_0056_Merge_Intervals.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -51,7 +52,7 @@ public class _0056_Merge_Intervals {
 
         List<Interval> res = new ArrayList<>();
         for (Interval interval : intervals) {
-            if (interval.getStart() <= interval.getEnd()) {
+            if (interval.getStart() <= end) {
                 end = Math.max(end, interval.getEnd());
             } else {
                 res.add(new Interval(start, end));
@@ -61,5 +62,36 @@ public class _0056_Merge_Intervals {
         }
         res.add(new Interval(start, end));
         return res;
+    }
+
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return intervals;
+        }
+        List<Interval> list = new ArrayList<>();
+        for (int[] interval : intervals) {
+            list.add(new Interval(interval[0], interval[1]));
+        }
+        list.sort(Comparator.comparing(Interval::getStart));
+        int start = list.get(0).getStart();
+        int end = list.get(0).getEnd();
+
+        List<Interval> res = new ArrayList<>();
+        for (Interval interval : list) {
+            if (interval.getStart() <= end) {
+                end = Math.max(end, interval.getEnd());
+            } else {
+                res.add(new Interval(start, end));
+                start = interval.getStart();
+                end = interval.getEnd();
+            }
+        }
+        res.add(new Interval(start, end));
+        int[][] r = new int[res.size()][2];
+        for (int i = 0; i < res.size(); i++) {
+            r[i][0] = res.get(i).getStart();
+            r[i][1] = res.get(i).getEnd();
+        }
+        return r;
     }
 }
