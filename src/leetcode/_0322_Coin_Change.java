ad0db8b54c81cc15a66aaf0a70b4bf621727af96
kimvra <787178256@qq.com>
2019-08-01 16:26:14 +0800

    leetcode 0322_Coin_Change 7.31 14:50

---
+++ src/leetcode/_0322_Coin_Change.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+
+/**
+ * Created by kimvra on 2019-08-01
+ */
+public class _0322_Coin_Change {
+    private int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
