53b5d8594b213a5d27877df162ee511f82d5ef59
kimvra <787178256@qq.com>
2019-08-02 10:31:03 +0800

    leetcode 0004_Median 8.2 10:30

---
+++ src/leetcode/_0004_Find_Median.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import org.junit.Test;
+
+/**
+ * Created by kimvra on 2019-08-02
+ */
+public class _0004_Find_Median {
+    private double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len = nums1.length + nums2.length;
+        if (len % 2 == 0) {
+            return (helper(nums1, 0, nums2, 0, len / 2) + helper(nums1, 0, nums2, 0, len / 2 + 1)) / 2.0;
+        }
+        return helper(nums1, 0, nums2, 0, (len + 1) / 2);
+    }
+
+    private int helper(int[] nums1, int m, int[] nums2, int n, int k) {
+        if (m >= nums1.length) return nums2[n + k - 1];
+        if (n >= nums2.length) return nums1[m + k - 1];
+        if (k == 1) return Math.min(nums1[m], nums2[n]);
+
+        int p1 = m + k / 2 - 1;
+        int p2 = n + k / 2 - 1;
+        int mid1 = p1 < nums1.length ? nums1[p1] : Integer.MAX_VALUE;
+        int mid2 = p2 < nums2.length ? nums2[p2] : Integer.MAX_VALUE;
+        if (mid1 < mid2) {
+            return helper(nums1, m + k / 2, nums2, n, k - k / 2);
+        }
+        return helper(nums1, m, nums2, n + k / 2, k - k / 2);
+    }
+
+    @Test
+    public void test() {
+        int[] nums1 = new int[]{1,2,3};
+        int[] nums2 = new int[]{4,5,6};
+        System.out.println(helper(nums1, 0, nums2, 0, 1));
+    }
+}
