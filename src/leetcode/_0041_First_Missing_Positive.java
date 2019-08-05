6df427e2c46682d84419288c0bb3bda100e40a7f
kimvra <787178256@qq.com>
2019-08-05 11:00:01 +0800

    ***findFirstMissingPositive 8.5 11:00***

---
+++ src/leetcode/_0041_First_Missing_Positive.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import org.junit.Test;
+
+/**
+ * Created by kimvra on 2019-08-05
+ */
+public class _0041_First_Missing_Positive {
+    private int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
