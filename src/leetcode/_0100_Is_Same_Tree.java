b8326fc3668eda1dc5457fb9c70a82ee3cf67a0e
kimvra <787178256@qq.com>
2019-09-08 16:24:10 +0800

    leetcode_0100_IsSameTree 9.8 16:24

---
+++ src/leetcode/_0100_Is_Same_Tree.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * Created by kimvra on 2019-09-08
+ */
+public class _0100_Is_Same_Tree {
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+}
