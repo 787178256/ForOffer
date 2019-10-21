4998cf2ef949fd172c35c5a1009c0c07678588ef
kimvra <787178256@qq.com>
2019-10-21 09:04:05 +0800

    leetcode_0108_Sorted_Array_To_BST 10.21 09:04

---
+++ src/leetcode/_0108_Sorted_Array_To_BST.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * Created by kimvra on 2019-10-21
+ */
+public class _0108_Sorted_Array_To_BST {
+    private TreeNode sortedArrayToBST(int[] nums) {
+        return nums == null ? null : buildTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildTree(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        int m = l + r >>> 1;
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = buildTree(nums, l, m - 1);
+        root.right = buildTree(nums, m + 1, r);
+        return root;
+    }
+}
